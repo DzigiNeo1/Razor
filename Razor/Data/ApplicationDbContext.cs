2df6d4ec123eb19c9f13153b5213eeaded060b7c
Ivann <Ivann@DESKTOP-UKID6DE>
2023-06-02 20:59:31 +0200

    Add project files.

---
+++ Razor/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Razor.Model;
+
+namespace Razor.Data;
+
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { 
+        
+            
+        }
+        public DbSet<Category>Category{ get; set; }   
+    }
+
